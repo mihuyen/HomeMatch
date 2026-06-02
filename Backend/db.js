74974c7f811393d1bf3f61695cb56ed09d7bf949
minquanle <quanml39205gmail.com>
2026-06-02 21:52:26 +0700

    fix header

--- Backend/db.js
+++ Backend/db.js
@@ -6,8 +6,8 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: '100420',
-    database: 'homematch',
+    password: 'Quan@134',
+    database: 'property_schema',
     waitForConnections: true,
     connectionLimit: 10
 });
